0af3b066311be2c24dc060071a583ac5febdc023
Itu <well.tree97@gmail.com>
2019-03-17 22:45:57 +0900

    fix,add:insertionsort の修正,アニメーションの追加

--- SortAnimation.py
+++ SortAnimation.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
-from tqdm import tqdm
+#from tqdm import tqdm
 
 COLOR = "black"
 N = 20
 VALUEMAX = 50
 VALUEMIN = 1
 def BubbleSort(x,y,ims):
-    for i in tqdm(range(0,N)):
+    for i in range(0,N):
         for j in range(i+1,N):
             if y[i] >= y[j]:
                 y[i],y[j] = y[j],y[i]
@@ -17,11 +17,11 @@ def BubbleSort(x,y,ims):
     return ims
 
 def InsertionSort(x,y,ims):
-    for i in range(0,N):
-        for j in range(i-1,1,-1):
+    for i in range(1,N):
+        for j in range(i-1,0,-1):
             if y[j-1] > y[j]:
-                y[i],y[j] = y[j],y[i]
-                ims.appned(plt.bar(x,y,color = COLOR))
+                y[j-1],y[j] = y[j],y[j-1]
+                ims.append(plt.bar(x,y,color = COLOR))
     return ims 
 
 fig = plt.figure()
@@ -30,8 +30,9 @@ x = [i+1 for i in range(N)]
 y = [random.randint(VALUEMIN,VALUEMAX) for i in range(N)]
 ims.append(plt.bar(x,y,color=COLOR))
 
-ims = InsertionSort(x,y,ims)
+#ims = InsertionSort(x,y,ims)
+ims = BubbleSort(x,y,ims)
 
 
 ani = animation.ArtistAnimation(fig, ims, interval = 100)
-ani.save("InsertSortanimation.mp4", writer="ffmpeg",dpi=300) 
+ani.save("SA_Bubble.mp4", writer="ffmpeg",dpi=300) 
